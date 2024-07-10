87e9a9b8800badcdac94624894ec32d7050123a5
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2024-07-10 12:53:06 -0400

    proyecto de js utilizando vite

---
+++ src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,12 @@
+/**
+ * funcion para obtener el valor de la carta
+ * @param {String} carta Carta obtenida del deck
+ * @returns {Number} Entrega el valor de la carta
+ */
+export const valorCarta = (carta) => {
+  if (!carta || carta.length === 0) {
+    throw "No hay carta";
+  }
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
